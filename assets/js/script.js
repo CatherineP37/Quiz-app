261c3c468982e18c6486f4c0f64e27ddc306bdb3
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-05-18 10:11:53 +0100

    Add questions array to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+    question: 'What does CSS stand for?',
+    answerChoices: ['Clean Spotty Socks','Clean Style Sheets','Cascading Style Sheets'],
+    correctAnswer: 'Cascading Style Sheets',
+},
+{
+    question: 'When was JavaScript invented?',
+    answerChoices: ['1995','1997','2003'],
+    correctAnswer: '1995',
+},
+]
